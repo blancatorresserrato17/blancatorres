0ac417594bc9f931ba1d5f914e7c1f955227097e
unknown <blancatorresserrato17@gmail.com>
2020-07-09 18:04:58 -0500

    cambios recientes

---
+++ FUNCIONES.js
@@ -0,0 +1,6 @@
+function cambiarColor() {
+  let etiqueta = document.getElementById("aqui")
+  etiqueta.classList.add("letrasrosas"<title>Color de texto</title>
+    <script type="text/javascript" >
+function cambiarColor(p) {
+<h1>Cambiar el color del texto</h1>
